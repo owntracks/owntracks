6c36654d91e307ff8ce93c3e3667e2732e511317
Christoph Krey <krey.christoph@gmail.com>
2015-07-06 18:16:35 +0200

    [NEW] new colors

--- assets/logos/resize.sh
+++ assets/logos/resize.sh
@@ -33,6 +33,7 @@ $INK -z -D -e "$MYPWD/$BASE-57.png" -f 		$MYPWD/$SVG -w 57 -h 57
 $INK -z -D -e "$MYPWD/$BASE-57@2x.png" -f 	$MYPWD/$SVG -w 114 -h 114
 $INK -z -D -e "$MYPWD/$BASE-60@2x.png" -f 	$MYPWD/$SVG -w 120 -h 120
 $INK -z -D -e "$MYPWD/$BASE-60@3x.png" -f 	$MYPWD/$SVG -w 180 -h 180
+$INK -z -D -e "$MYPWD/$BASE-64.png" -f 		$MYPWD/$SVG -w 64 -h 64
 $INK -z -D -e "$MYPWD/$BASE-72.png" -f 		$MYPWD/$SVG -w 72 -h 72
 $INK -z -D -e "$MYPWD/$BASE-72@2x.png" -f 	$MYPWD/$SVG -w 144 -h 144
 $INK -z -D -e "$MYPWD/$BASE-76.png" -f 		$MYPWD/$SVG -w 76 -h 76 
@@ -40,6 +41,7 @@ $INK -z -D -e "$MYPWD/$BASE-76@2x.png" -f 	$MYPWD/$SVG -w 152 -h 152
 $INK -z -D -e "$MYPWD/$BASE-86@2x.png" -f 	$MYPWD/$SVG -w 172 -h 172
 $INK -z -D -e "$MYPWD/$BASE-98@2x.png" -f 	$MYPWD/$SVG -w 196 -h 196
 $INK -z -D -e "$MYPWD/$BASE-192.png" -f 	$MYPWD/$SVG -w 192 -h 192
+$INK -z -D -e "$MYPWD/$BASE-320.png" -f 	$MYPWD/$SVG -w 320 -h 320 
 $INK -z -D -e "$MYPWD/$BASE-512.png" -f 	$MYPWD/$SVG -w 512 -h 512
 $INK -z -D -e "$MYPWD/$BASE-1024.png" -f 	$MYPWD/$SVG -w 1024 -h 1024
 $IMAGEW "$MYPWD/$BASE-512.png" "$MYPWD/$BASE-512-noalpha.png"
