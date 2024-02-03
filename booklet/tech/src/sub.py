525af87af096c535b4687d385327fc1b9908836c
OwnTracks (via TravisCI) <travis-ci@owntracks.org>
2024-02-03 16:38:48 +0000

    TravisCI rebuild

--- booklet/tech/src/sub.py
+++ booklet/tech/src/sub.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 import json
 
 # The callback for when the client successfully connects to the broker
-def on_connect(client, userdata, rc):
+def on_connect(client, userdata, flags, rc):
     ''' We subscribe on_connect() so that if we lose the connection
         and reconnect, subscriptions will be renewed. '''
 
@@ -15,18 +15,21 @@ def on_message(client, userdata, msg):
 
     topic = msg.topic
 
-    try:
-        data = json.loads(str(msg.payload))
+    payload = msg.payload.decode("utf-8")
 
-        print "TID = {0} is currently at {1}, {2}".format(data['tid'], data['lat'], data['lon'])
+    try:
+        data = json.loads(payload)
+
+        print("TID = {0} is currently at {1}, {2}".format(data['tid'], data['lat'], data['lon']))
     except:
-        print "Cannot decode data on topic {0}".format(topic)
+        print("Cannot decode data on topic {0}".format(topic))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+#client.connect("localhost", 1883, 60)
+client.connect("192.168.1.140", 1883, 60)
 
 # Blocking call which processes all network traffic and dispatches
 # callbacks (see on_*() above). It also handles reconnecting.
